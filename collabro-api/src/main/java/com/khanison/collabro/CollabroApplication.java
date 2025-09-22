80e6bbce73fde3290c132b07f304dc2021e2cfd4
Khanison Kheereesantikul <khanison.kheereesantikul@etu.univ-lyon1.fr>
2025-09-22 18:57:38 +0200

    setup backend with Spring Boot, Maven, Docker

---
+++ collabro-api/src/main/java/com/khanison/collabro/CollabroApplication.java
@@ -0,0 +1,13 @@
+package com.khanison.collabro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabroApplication.class, args);
+	}
+
+}
---
+++ collabro-api/src/test/java/com/khanison/collabro/CollabroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanison.collabro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
